9fc0e12ddb84f93b8d616ca1ab02c0bb295ac3f0
= <freebirdhennessy@gmail.com>
2019-03-02 13:43:04 -0500

    Range selector and slider

--- src/index.js
+++ src/index.js
@@ -32,28 +32,56 @@ class Charts extends React.Component {
         x: ['2013-10-04 08:00:00', '2013-10-04 10:15:00', '2013-10-04 12:00:00'],
         y: [15, 10, 8],
         type: 'scatter',
-        name: 'Home to work' 
+        name: 'Home to work',
+        mode: "lines"
       },
       workToGymTrace : {
         x: ['2013-10-04 08:15:00', '2013-10-04 10:06:00', '2013-10-04 12:55:00'],
         y: [12, 7, 10],
         type: 'scatter',
-        name: 'Work to gym'
+        name: 'Work to gym',
+        mode: "lines"
       },
       gymToHomeTrace : {
         x: ['2013-10-04 08:05:00', '2013-10-04 10:16:00', '2013-10-04 12:15:00'],
         y: [15, 7, 12],
         type: 'scatter',
-        name: 'Gym to home'
+        name: 'Gym to home',
+        mode: "lines"
       }
     }
   }
   render() {
+    var selectorOptions = {
+      buttons: [
+        {
+          step: "hour",
+          stepmode: "backward",
+          count: 1,
+          label: "Past hour"
+        },
+        {
+          step: "day",
+          stepmode: "todate",
+          count: 1,
+          label: "Today"
+        },
+        {
+          step: "all"
+        }
+      ]};
     var layout = {
+      title: "Commute Time",
+      xaxis: {
+        rangeselector: selectorOptions,
+        rangeslider: {
+        }
+      },
       yaxis: {
         range: [0, 20]
       }
     };
+
     return (
       <Plot
         data={[this.state.homeToWorkTrace, 
